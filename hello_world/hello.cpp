562fee2c19f31e48d917274fe596aaa7c88da42a
Neelesh <neelesh.vishwakarma235@gmail.com>
2021-09-05 17:08:40 +0530

    First C++

---
+++ hello_world/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+
+    int favourite_number = 0;
+
+    std::cout<<"Enter your favourite number between 1 and 100:";
+    std::cin>>favourite_number;
+
+    std::cout<<"Amazing! That's my favourite number too."<<std::endl;
+
+    return 0;
+}
