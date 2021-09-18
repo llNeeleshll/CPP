d886091df86ce3fd4a4014115c1e47c748a2589c
Neelesh <neelesh.vishwakarma235@gmail.com>
2021-09-18 18:00:07 +0530

    Dynamic Array creation via pointers done

--- pointers/dynamic_variable.cpp
+++ pointers/dynamic_variable.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+int *create_array(int size, int value);
+void display_array(const int *array, int size);
+
 int main(){
 
     int *int_array {};
@@ -10,21 +13,44 @@ int main(){
     cout<<"What would be the length of array? ";
     cin>>size;
 
-    int_array = new int[size];
+    // int_array = new int[size];
 
-    cout<<"Now enter the values followed by enter : ";
+    // cout<<"Now enter the values followed by enter : ";
 
-    for(int i {0};i<size;i++){
-        cin>>int_array[i];
-    }
+    // for(int i {0};i<size;i++){
+    //     cin>>int_array[i];
+    // }
 
-    cout<<"You'd entered : ";
+    // cout<<"You'd entered : ";
 
-    for(int i {0};i<size;i++){
-        cout<<int_array[i]<<" ";
-    }
+    // for(int i {0};i<size;i++){
+    //     cout<<int_array[i]<<" ";
+    // }
+
+    int_array = create_array(size, 20);
+
+    display_array(int_array, size);
 
     delete [] int_array;
 
     return 0;
+}
+
+int *create_array(int size, int value){
+    int *array {};
+    array = new int[size];
+
+    for(int i {};i<size;i++){
+        *(array + i) = value;
+    }
+
+    return array;
+}
+
+void display_array(const int *array, int size){
+    cout<<"Following is the array : ";
+    for(int i {};i<size;i++){
+        cout<<*(array + i)<<" ";
+    }
+    cout<<endl;
 }
