b22dea6003a3de99c25367c3bc2631374f255f28
Neelesh <neelesh.vishwakarma235@gmail.com>
2021-09-24 16:07:40 +0530

    Challange code done

---
+++ inheritance/challange/Account.h
@@ -0,0 +1,53 @@
+// Simple Account 
+#ifndef _ACCOUNT_H_
+#define _ACCOUNT_H_
+#include <iostream>
+#include <string>
+
+class Account {
+    friend std::ostream &operator<<(std::ostream &os, const Account &account);
+private:   
+    static constexpr const char *def_name = "Unnamed Account";
+    static constexpr double def_balance = 0.0;
+protected:
+    std::string name;
+    double balance;
+public:
+    Account(std::string name = def_name, double balance = def_balance);
+    // Account(std::string name = "Unamed Account", double balance = 0.0);
+    bool deposit(double amount);
+    bool withdraw(double amount);
+    double get_balance() const;
+};
+
+Account::Account(std::string name, double balance) 
+    : name{name}, balance{balance} {
+}
+
+bool Account::deposit(double amount) {
+    if (amount < 0) 
+        return false;
+    else {
+        balance += amount;
+        return true;
+    }
+}
+
+bool Account::withdraw(double amount) {
+    if (balance-amount >=0) {
+        balance-=amount;
+        return true;
+    } else
+        return false;
+}
+
+double Account::get_balance() const {
+    return balance;
+}
+
+std::ostream &operator<<(std::ostream &os, const Account &account) {
+    os << "[Account: " << account.name << ": " << account.balance << "]";
+    return os;
+}
+
+#endif
